e08e95493a42e45d8057bd0ff9265067d5af64ca
Joe <jhhong0930@gmail.com>
2021-08-28 20:33:02 +0900

    Create project

---
+++ src/main/java/com/kh/conimal/ConimalApplication.java
@@ -0,0 +1,13 @@
+package com.kh.conimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConimalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConimalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/conimal/ConimalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.conimal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConimalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
