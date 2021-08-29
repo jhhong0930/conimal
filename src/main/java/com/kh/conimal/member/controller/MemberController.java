82111df1d4f004cad0a73970e314dd5e1998aa3e
Joe <jhhong0930@gmail.com>
2021-08-29 19:26:55 +0900

    Update: 로그인 기능 구현, 메인페이지 작동

--- src/main/java/com/kh/conimal/member/controller/MemberController.java
+++ src/main/java/com/kh/conimal/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.kh.conimal.member.controller;
 
 import com.kh.conimal.member.model.service.MemberService;
 import com.kh.conimal.member.model.vo.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class MemberController {
 
     @Autowired private MemberService service;
@@ -21,11 +23,16 @@ public class MemberController {
     @PostMapping("/login.me")
     public String loginMember(Member member, Model model) {
 
+        log.info("memId = {}", member.getMemId());
+        log.info("memPwd = {}", member.getMemPwd());
+
         Member loginMember = service.loginMember(member);
+
+        log.info("loginMember = {}", loginMember);
 
         model.addAttribute("loginMember", loginMember);
 
-        return "fragement/layout";
+        return "mainPage";
     }
 
 }
